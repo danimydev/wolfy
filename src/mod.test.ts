d337922b46b89189b25526cc28387e1c0ccb6cf4
Daniel Madrid <danimydev@gmail.com>
2024-03-25 18:51:43 -0500

    rm tests

--- src/mod.test.ts
+++
@@ -1,6 +0,0 @@
-import { greet } from "@/mod.ts";
-import { assertEquals } from "assert";
-
-Deno.test("greet", () => {
-  assertEquals(greet(), "Hello World");
-});
--- src/mod.ts
+++ src/mod.ts
@@ -1,9 +1,50 @@
 /**
- * A simple function that returns a string
- * @returns Hello World
+ * Options for configuring a query to the SimpleApi.
  */
-export function greet(): string {
-  return "Hello World";
+export interface QueryOptions {
+  layout?: "divider" | "labelbar";
+  background?: string;
+  foreground?: "white" | "black";
+  fontsize?: number;
+  width?: number;
+  units?: "metric" | "imperial";
+  timeout?: number;
 }
 
-export { SimpleApi } from "@/simple-api.ts";
+export class SimpleApi {
+  private baseUrl: string;
+
+  /**
+   * Constructs a SimpleApi object.
+   * @param {Object} params - The parameters for constructing the SimpleApi.
+   * @param {string} params.appId - The Wolfram Alpha app ID.
+   */
+  constructor({ appId }: { appId: string }) {
+    /**
+     * The base URL for the API.
+     * @type {string}
+     * @private
+     */
+    this.baseUrl = `http://api.wolframalpha.com/v1/simple?appid=${appId}`;
+  }
+
+  /**
+   * Queries the API with the provided input and options.
+   * @param {Object} params - The parameters for the query.
+   * @param {string} params.input - The input for the query.
+   * @param {QueryOptions} [params.options] - The options for the query.
+   * @returns {Promise<Blob>} A promise resolving to the query result as a Blob.
+   */
+  async query(
+    { input, options }: { input: string; options?: QueryOptions },
+  ): Promise<Blob> {
+    let queryParams = `&i=${input}`;
+    const entries = Object.entries(options || {});
+    for (const entry of entries) {
+      queryParams += `&${entry.at(0)}=${entry.at(1)}`;
+    }
+    const res = await fetch(encodeURI(`${this.baseUrl}${queryParams}`));
+
+    return await res.blob();
+  }
+}
